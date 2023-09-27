161fc28be79b5213e08b5e6a0463427309cb5017
fernandaeana <144800263+fernandaeana@users.noreply.github.com>
2023-09-27 09:43:02 -0300

    Add files via upload

--- main.js
+++ main.js
@@ -35,14 +35,11 @@ function tocaSomTom(){
      document.querySelector('.tecla_tom').onclick = tocaSomTom;}
      document.querySelectorAll('.tecla');
      const listaDeTeclas = document.querySelectorAll('.tecla');
-     
-listaDeTeclas[0].onclick = tocaSomPom;
-listaDeTeclas[0].onclick = tocaSomClap;
-listaDeTeclas[0].onclick = tocaSomTim;
-listaDeTeclas[0].onclick = tocaSomPuff;
-listaDeTeclas[0].onclick = tocaSomSplash;
-listaDeTeclas[0].onclick = tocaSomToim;
-listaDeTeclas[0].onclick = tocaSomPsh;
-listaDeTeclas[0].onclick = tocaSomTic;
-listaDeTeclas[0].onclick = tocaSomTom;
+
+// estrair de repetição - Enquanto
+while(contador < listaDeTeclas.length) {
+    let contador = 0;
+    listaDeTeclas[contador].onclick = tocaSomPom;
+    contador = contador+1;
+}
 
